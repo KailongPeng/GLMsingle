773917d3bfe02a9786f00b2a00e42ab8a7a92958
kailong <kailongpeng001@gmail.com>
2022-05-24 01:49:29 -0400

    python version 3.8 to try on example1

--- examples/example1.sh
+++ examples/example1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -p psych_day,psych_gpu,psych_scavenge,psych_week
-#SBATCH --job-name=glmsingle
+#SBATCH --job-name=example1
 #SBATCH --ntasks=1 --nodes=1
 #SBATCH --time=6:00:00
-#SBATCH --output=logs/%A_%a.out
+#SBATCH --output=logs/%J.out
 #SBATCH --mem=100g
 #SBATCH --requeue
 
@@ -11,3 +11,6 @@ set -e
 module load FSL/5.0.10
 . /nexsan/apps/hpc/Apps/FSL/5.0.10/etc/fslconf/fsl.sh
 python -u /gpfs/milgram/project/turk-browne/projects/localize/analysis/GLMsingle/examples/example1.py
+
+#cd /gpfs/milgram/project/turk-browne/projects/localize/analysis/GLMsingle/examples/
+#sbatch example1.sh
